a58e5fba8e24f24a0e73155ca0548c65a7603dd7
qwa-rwansouza <rwan.souza@qwasolucoes.com.br>
2023-09-15 09:21:44 -0300

    teste de conversao de funcionario para string

--- src/test/java/br/com/qwasolucoes/mentoria/testes/LogicaProgramacao.java
+++ src/test/java/br/com/qwasolucoes/mentoria/testes/LogicaProgramacao.java
@@ -7,23 +7,31 @@ import org.junit.Test;
 
 import br.com.qwasolucoes.mentoria.implementacoes.logica_programacao.LogicaProgramacaoProvider;
 import br.com.qwasolucoes.mentoria.model.Funcionario;
-import br.com.qwasolucoes.mentoria.testes.logica_programacao.LogicaProgramacaoTest;
 import br.com.qwasolucoes.mentoria.testes.mock.FuncionarioMock;
 
-public class LogicaProgramacao implements LogicaProgramacaoTest {
-
+public class LogicaProgramacao {
+	
+	private LogicaProgramacaoProvider logicaProgramacao = new LogicaProgramacaoProvider();
+	
 	@Test
 	public void conversaoStringParaPessoaTest() {
 		
-		LogicaProgramacaoProvider logicaProgramacao = new LogicaProgramacaoProvider();
-		
-		
 		// Lógica implantada pela pessoa mentorada
-		List<Funcionario> conversaoStringParaPessoa = logicaProgramacao.conversaoStringParaPessoa(FuncionarioMock.funcionariosMockString());
+		List<Funcionario> conversaoStringParaPessoa = this.logicaProgramacao.conversaoStringParaPessoa(FuncionarioMock.funcionariosMockString());
 		
+		// Método  de mock para comparar com o retorno do método implementado pela pessoa mentorada
 		List<Funcionario> funcionarioCompare = FuncionarioMock.funcionarios();
 		
 		Assert.assertEquals(conversaoStringParaPessoa, funcionarioCompare);
+	}
+	
+	@Test 
+	public void conversaoPessoaParaString() {
 		
+		List<Funcionario> funcionarios = this.logicaProgramacao.conversaoStringParaPessoa(FuncionarioMock.funcionariosMockString());
+		
+		List<String> conversaoPessoaParaString = this.logicaProgramacao.conversaoPessoaParaString(funcionarios);
+		
+		Assert.assertEquals(funcionarios, conversaoPessoaParaString);
 	}
 }
